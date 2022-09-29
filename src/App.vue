ff0de4889b75b23a2934c7f8698c21601b91ccb2
LeeYoungJe <zofhd2@naver.com>
2022-09-29 16:34:21 +0900

    Menu,item list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="menuItems in menu" :key="menuItems">{{ menuItems }}</a>
+  </div>
+  <div v-for="(productItems, idx)  in 3" :key="productItems">
+    <img src="@/assets/room0.jpg">
+    <h4>{{ products[idx] }} 원룸</h4>
+    <p>{{ price[idx] }} 만원</p>
+    <button @click="increase(idx);">허위매물신고</button><span>신고수 : {{counts[idx]}} </span>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+
+      counts: [0,0,0],
+      menu: ['Home', 'Shop', 'About'],
+      price: ['60', '50', '40'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  methods: {
+    increase(idx){
+     this.counts[idx] += 1;
+     console.log(this);
+    },
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
